c1749ad6996777c1fccf94623f5bb6a30c5d7264
srajansohani <99042007+srajansohani@users.noreply.github.com>
2025-12-29 15:55:20 +0530

    Fixed order of external connections and added heartbeat

--- controllers/problem_submission_controller.js
+++ controllers/problem_submission_controller.js
@@ -3,45 +3,45 @@ import { User } from "../models/User.js";
 import { Problem } from "../models/Problem.js";
 
 export const handel_problem_submission = async (submission) => {
-    const submission_id = submission._id;
+  const submission_id = submission._id;
 
-    await Submission.findByIdAndUpdate(submission_id, submission);
+  await Submission.findByIdAndUpdate(submission_id, submission);
 
-    if (submission.error) {
-        if (!submission.throwaway) {
-            const problem = await Problem.findById(submission.problem_id);
-            problem.wrong_submissions += 1;
-            await problem.save();
-            const user = await User.findById(submission.user_id);
-            user.submissions
-                ? user.submissions.push(submission_id)
-                : (user.submissions = [submission_id]);
-            await user.save();
-        }
-
-        return;
-    }
-
+  if (submission.error) {
     if (!submission.throwaway) {
-        const problem = await Problem.findById(submission.problem_id);
-        const user = await User.findById(submission.user_id);
-        user.submissions
-            ? user.submissions.push(submission_id)
-            : (user.submissions = [submission_id]);
-        if (
-            submission.result === "AC" &&
-            (!user.solved_problems ||
-                !user.solved_problems.includes(submission.problem_id))
-        ) {
-            user.solved_problems
-                ? user.solved_problems.push(submission.problem_id)
-                : (user.solved_problems = [submission.problem_id]);
-            problem.correct_submissions += 1;
-        }
-        if (submission.result == "WA") {
-            problem.wrong_submissions += 1;
-        }
-        await user.save();
-        await problem.save();
+      const problem = await Problem.findById(submission.problem_id);
+      problem.wrong_submissions += 1;
+      await problem.save();
+      const user = await User.findById(submission.user_id);
+      user.submissions
+        ? user.submissions.push(submission_id)
+        : (user.submissions = [submission_id]);
+      await user.save();
     }
+
+    return;
+  }
+
+  if (!submission.throwaway) {
+    const problem = await Problem.findById(submission.problem_id);
+    const user = await User.findById(submission.user_id);
+    user.submissions
+      ? user.submissions.push(submission_id)
+      : (user.submissions = [submission_id]);
+    if (
+      submission.result === "AC" &&
+      (!user.solved_problems ||
+        !user.solved_problems.includes(submission.problem_id))
+    ) {
+      user.solved_problems
+        ? user.solved_problems.push(submission.problem_id)
+        : (user.solved_problems = [submission.problem_id]);
+      problem.correct_submissions += 1;
+    }
+    if (submission.result == "WA") {
+      problem.wrong_submissions += 1;
+    }
+    await user.save();
+    await problem.save();
+  }
 };
--- index.js
+++ index.js
@@ -11,101 +11,111 @@ dotenv.config();
 export let redisClient;
 
 const handel_submission = (submission_data) => {
-    if (submission_data.type == "problem_submission") {
-        handel_problem_submission(submission_data.submission);
-        return;
-    }
+  if (submission_data.type == "problem_submission") {
+    handel_problem_submission(submission_data.submission);
+    return;
+  }
 
-    if (submission_data.type == "playground_submission") {
-        handel_playground_submission(submission_data.submission);
-        return;
-    }
+  if (submission_data.type == "playground_submission") {
+    handel_playground_submission(submission_data.submission);
+    return;
+  }
 
-    if (submission_data.type == "contest_submission") {
-        handel_contest_submission(
-            submission_data.submission,
-            submission_data.submission.contest_id
-        );
-        return;
-    }
+  if (submission_data.type == "contest_submission") {
+    handel_contest_submission(
+      submission_data.submission,
+      submission_data.submission.contest_id
+    );
+    return;
+  }
 
-    console.log(`submission of type ${submission_data.type} not supported`);
+  console.log(`submission of type ${submission_data.type} not supported`);
 };
 
 const connect_to_mongoDB = async () => {
-    try {
-        mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to mongoDB database");
-    } catch (err) {
-        throw err;
-    }
+  try {
+    mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to mongoDB database");
+  } catch (err) {
+    throw err;
+  }
 };
 
 const connect_to_rabbitMQ = async () => {
-    try {
-        amqp.connect(process.env.RABBIT_MQ_URI, function (error, connection) {
-            if (error) {
-                throw error;
+  try {
+    amqp.connect(
+      process.env.RABBIT_MQ_URI,
+      {
+        heartbeat: 60,
+      },
+      function (error, connection) {
+        if (error) {
+          throw error;
+        }
+
+        connection.createChannel(function (error, channel) {
+          if (error) {
+            throw error;
+          }
+
+          channel.assertQueue("processed_submission", {
+            durable: false,
+          });
+
+          console.log("waiting for messages...");
+
+          channel.consume(
+            "processed_submission",
+            function (msg) {
+              const submission_data = JSON.parse(msg.content.toString());
+
+              handel_submission(submission_data);
+            },
+            {
+              noAck: true,
             }
-
-            connection.createChannel(function (error, channel) {
-                if (error) {
-                    throw error;
-                }
-
-                channel.assertQueue("processed_submission", {
-                    durable: false,
-                });
-
-                console.log("waiting for messages...");
-
-                channel.consume(
-                    "processed_submission",
-                    function (msg) {
-                        const submission_data = JSON.parse(
-                            msg.content.toString()
-                        );
-
-                        handel_submission(submission_data);
-                    },
-                    {
-                        noAck: true,
-                    }
-                );
-            });
+          );
         });
+      }
+    );
 
-        console.log("Connected to rabbitMQ");
-    } catch (err) {
-        throw err;
-    }
+    console.log("Connected to rabbitMQ");
+  } catch (err) {
+    throw err;
+  }
 };
 
 const connect_to_redis = async () => {
-    const client = redis.createClient({
-        password: process.env.REDIS_PASSWORD,
-        socket: {
-            host: process.env.REDIS_URI,
-            port: 13987,
-        },
-    });
+  const client = redis.createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+      host: process.env.REDIS_URI,
+      port: process.env.REDIS_PORT,
+      heartbeatInterval: 60000,
+    },
+    heartbeatInterval: 60000,
+  });
 
-    client.on("error", (err) => console.log("Redis Client Error", err));
+  client.on("error", (err) => console.log("Redis Client Error", err));
 
-    await client.connect();
+  await client.connect();
 
-    // await client.set("key", "value");
-    // const value = await client.get("key");
-    // console.log("Test Key Value:", value);
+  // await client.set("key", "value");
+  // const value = await client.get("key");
+  // console.log("Test Key Value:", value);
 
-    redisClient = client;
-    console.log("connected to redis");
+  redisClient = client;
+  console.log("connected to redis");
 };
 
 const startup = async () => {
-    await connect_to_redis();
+  try {
     await connect_to_mongoDB();
     await connect_to_rabbitMQ();
+    await connect_to_redis();
+  } catch (err) {
+    console.log("Error during startup:", err);
+  }
 };
 
 startup();
